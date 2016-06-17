3bbaab73000dc48e0e4532e64117ef1e04a4e59a
Seumus <seumusblair@gmail.com>
2016-06-17 11:15:26 +0100

    server + brach

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var MongoClient = require('mongodb').MongoClient
+
+// Connection URL
+var url = 'mongodb://localhost:27017/bank';
