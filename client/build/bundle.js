c50c3bed65bbad473bed3eb0d3c0ba502df9cb5c
Wojtek Tartanus <wtartanus@gmail.com>
2016-06-20 11:17:49 +0100

    changed api requst

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -61,7 +61,7 @@
 	};
 	
 	var getSectors = function(companies) {
-	  var sectorsAll = []
+	  var sectorsAll = []   
 	  for( company of companies ) {
 	    sectorsAll.push(company.Sector);
 	  } 
@@ -117,13 +117,13 @@
 	  var symbol = this.id;
 	
 	  var request = new XMLHttpRequest();
-	  var url = "http://finance.yahoo.com/webservice/v1/symbols/"+symbol+"/quote?format=json&view=detail";
+	  var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22"+symbol+"%22%20and%20startDate%20%3D%20%222015-06-20%22%20and%20endDate%20%3D%20%222016-06-20%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
 	    request.open("GET", url);
 	    request.onload = function() {
 	      if( request.status === 200 ) {
 	        console.log("HI");
 	        var result = JSON.parse(request.responseText);
-	        var result = result.list.resources[0].resource.fields;
+	        var result = result.query.results.quote;
 	        console.log(result);
 	      }
 	    }
