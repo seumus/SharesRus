15772af8923158fe038aa9f4785f335ebdf9b8c1
Your Name <you@example.com>
2016-06-21 15:52:14 +0100

    updated tables

--- client/src/portfolio/specs/stock_test.js
+++ client/src/portfolio/specs/stock_test.js
@@ -3,10 +3,7 @@ var assert = require ('chai').assert;
 
 describe('Stock', function(){
 
-  it("has an empty array for shares to be added", function(){
-    var portfolio = new Portfolio()
-    assert.deepEqual([], portfolio.shares)
-  }),
+
 
 
 
