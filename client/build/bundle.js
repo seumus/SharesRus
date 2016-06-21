9e86dbeb57966bf057ef2afeee45e9409960d4ec
Seumus <seumusblair@gmail.com>
2016-06-21 10:46:50 +0100

    stupid fucking webpack

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -433,29 +433,14 @@
 
 	var Stock = function(params) {
 	  this.name = params.name;
-	  // this.epic = params.epic;
-	  // this.price = params.price;
-	  // this.quantity = params.quantity;
-	  // this.buyPrice = params.buyPrice;
-	  // this.pastCloseOfDayPrices = params.pastCloseOfDayPrices;
-	  // this.buyDate = params.buyDate;
+	  this.epic = params.epic;
+	  this.price = params.price;
+	  this.quantity = params.quantity;
+	  this.buyPrice = params.buyPrice;
+	  this.pastCloseOfDayPrices = params.pastCloseOfDayPrices;
+	  this.buyDate = params.buyDate;
 	};
 	
-	
-	
-	Stock.prototype = {
-	  save: function(){
-	    var url = 'http://localhost:3000/market';
-	    var request = new XMLHttpRequest();
-	    request.open("POST", url);
-	    request.setRequestHeader("Content-Type", "application/json");
-	    request.onload = function(){
-	      if(request.status === 200){
-	      }
-	    }
-	    request.send(JSON.stringify(this));
-	  }
-	}
 	module.exports = Stock;
 
 
@@ -473,17 +458,6 @@
 	Portfolio.prototype = {
 	  addStock: function(share){
 	    this.shares.push(share);
-	  },
-	  save: function(){
-	    var url = 'http://localhost:3000/shares';
-	    var request = new XMLHttpRequest();
-	    request.open("POST", url);
-	    request.setRequestHeader("Content-Type", "application/json");
-	    request.onload = function(){
-	      if(request.status === 200){
-	      }
-	    }
-	    request.send(JSON.stringify(this));
 	  }
 	};
 	
@@ -491,6 +465,7 @@
 	
 	
 	module.exports = Portfolio;
+	
 
 
 /***/ },
